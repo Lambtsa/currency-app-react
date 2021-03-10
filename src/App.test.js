b96ac250937b5c2402622cf1d32d059508f0feb8
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-10 17:51:58 +0100

    initial setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('The App component', () => {
+  test('renders a H1 with with the text Hello World!', () => {
+    const { container } = render(<App />);
+    const header = screen.getByRole('heading');
+    expect(header).toBeInTheDocument();
+    expect(container.querySelector('h1')).toBeInTheDocument();
+    expect(container.querySelector('h1')).toHaveTextContent('Hello World!')
+  });
 });
