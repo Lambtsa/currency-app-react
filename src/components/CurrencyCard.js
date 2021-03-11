e251dada2bd4399cbca4cf4506f213078f425999
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-11 22:31:37 +0100

    update

--- src/components/CurrencyCard.js
+++ src/components/CurrencyCard.js
@@ -3,15 +3,28 @@ import heart from '../assets/heart.svg';
 import heartLiked from '../assets/heart__liked.svg';
 
 const CurrencyCard = ({ details }) => {
-  const [liked, setLiked] = useState(false);
+  const [like, setLike] = useState(false);
 
   const handleBtnClick = () => {
-    setLiked(!liked);
+    setLike(!like);
+    if (localStorage.likes) {
+      const likes = JSON.parse(localStorage.likes)
+        .filter(e => e.initials !== details.initials);
+      localStorage.setItem('likes', JSON.stringify([...likes, {
+        initials: details.initials,
+        liked: !like,
+      }]));
+    } else {
+      localStorage.setItem('likes', JSON.stringify([{
+        initials: details.initials,
+        liked: !like,
+      }]));
+    }
   };
 
   return (
     <>
-      <article className={`article order-${liked ? '1' : '2'}`}>
+      <article className={`article order-${like ? '1' : '2'}`}>
         <div className="article__details">
           <img className="article__img" src={`http://localhost:8080${details.logo}`} alt="" />
           <div className="article__content">
@@ -21,7 +34,7 @@ const CurrencyCard = ({ details }) => {
         </div>
         <div className="article__rates">
           <button className="article__btn" type="button" onClick={handleBtnClick}>
-            <img className="article__like" src={liked ? heartLiked : heart} alt={liked ? 'Heart showing that currency is not liked' : 'Red heart showing currency is liked'} />
+            <img className="article__like" src={like ? heartLiked : heart} alt={like ? 'Heart showing that currency is not liked' : 'Red heart showing currency is liked'} />
           </button>
           <p className="article__subtitle">{details.rate}</p>
         </div>
