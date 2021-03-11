839bbbc9dd4bc235568deeb142f5c50ab1d3baf2
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-11 15:58:35 +0100

    envs

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -1,4 +1,8 @@
-const fetchData = () => fetch('http://localhost:8080/currencies')
+const server = process.env.NODE_ENV === 'production'
+  ? 'https://currency-app-api.herokuapp.com/currencies'
+  : 'http://localhost:8080/currencies';
+
+const fetchData = () => fetch(server)
   .then(response => response.json());
 
 export default fetchData;
